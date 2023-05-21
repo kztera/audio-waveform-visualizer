aaa7c796cdc66b5dc7843db73209d391ede9c60a
Nguyen Khoa <116504460+kztera@users.noreply.github.com>
2023-05-21 10:21:56 +0700

    ⚡️ delete comment code

--- pages/lib/Shaders.js
+++ pages/lib/Shaders.js
@@ -18,15 +18,10 @@ const vertexShader = () => {
         float floor_x = round(x);
 	      float floor_y = round(y);
 
-        float x_multiplier = (40.0 - x) / 8.0;
-        float y_multiplier = (40.0 - y) / 8.0;
-
-        // z = position.z;
-        // z = abs(position.x) + abs(position.y);
-        // z = sin(abs(position.x) + abs(position.y));
-        // z = sin(abs(position.x) + abs(position.y) + u_time * .005);
+        // z = sin(u_data_arr[int(floor_x)] / 50.0 - u_data_arr[int(floor_y)] / 50.0) * u_amplitude;
+        // z = u_data_arr[int(floor_x)] / 50.0 + u_data_arr[int(floor_y)] / 50.0;
+        // z = u_data_arr[int(floor_x)] / 1.0 + u_data_arr[int(floor_y)] / 1.0; 
         z = sin(u_data_arr[int(floor_x)] / 50.0 + u_data_arr[int(floor_y)] / 50.0) * u_amplitude;
-        // z = (u_data_arr[int(floor_x)] / 50.0 + u_data_arr[int(floor_y)] / 50.0) * 2.0;
 
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, z, 1.0);
       }
@@ -37,11 +32,11 @@ const fragmentShader = () => {
 	return `
     varying float x;
     varying float y;
-    varying float z;
-    varying vec3 vUv;
+
+    float rand = 35.0;
 
     void main() {
-      gl_FragColor = vec4((40.0 - abs(x)) / 40.0, (40.0 - abs(y)) / 40.0, (abs(x + y) / 2.0) / 40.0, 1.0);
+      gl_FragColor = vec4((rand - abs(x)) / rand, (rand - abs(y)) / rand, (abs(x + y) / 2.0) / rand, 1.0);
     }
   `;
 };
